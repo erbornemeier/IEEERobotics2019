f5ee80d06bac65dcabbd6ff3b29ed7e3af404996
ZKasica <zkasica@mymail.mines.edu>
2018-10-20 18:21:29 -0600

    Work on tracking blue object/LED

--- src/servo_tracking/servo_tracking.ino
+++ src/servo_tracking/servo_tracking.ino
@@ -5,8 +5,14 @@
 
 #define SERVO_PIN 11
 
+#define CAM_FRAME_WIDTH 480
+#define MIN_CAM_X -CAM_FRAME_WIDTH / 2
+#define MAX_CAM_X CAM_FRAME_WIDTH / 2
+#define SERVO_MIN 0
+#define SERVO_MAX 180
+
 Servo servo;
-int servoRotation = 5;
+volatile int servoRotation = 90;
 
 void setup() {
   servo.attach(SERVO_PIN);
@@ -20,16 +26,33 @@ void setup() {
 }
 
 void loop() {
-
+//    Serial.println(servoRotation);
+    servo.write(servoRotation);
 }
 
 // numBytes: total number of bytes
 // First byte: flag
 // Second byte: number of data bytes
 void onReceiveI2C(int numBytes) {
+  
   while(Wire.available()) {
+    Wire.read();
+    Wire.read();
+    
     int value = get16BitInt();
     Serial.println(value);
+
+//    int servoRotation = map(value, MIN_CAM_X, MAX_CAM_X, SERVO_MIN, SERVO_MAX);
+//    servo.write(servoRotation);
+    if(value < 0) {
+      if(servoRotation < 180) {
+        servoRotation += 2;
+      }
+    } else if(value > 0) {
+      if(servoRotation > 0) {
+        servoRotation -= 2;
+      }  
+   }
   }
 }
 
