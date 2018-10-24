68aee337f0521dac141b354dcbf0d0e7ba66b00b
ZKasica <zkasica@mymail.mines.edu>
2018-10-23 21:12:24 -0600

    Get beacon tracking working

--- src/servo_tracking/servo_tracking.ino
+++ src/servo_tracking/servo_tracking.ino
@@ -6,13 +6,14 @@
 #define SERVO_PIN 11
 
 #define CAM_FRAME_WIDTH 480
+#define CAM_FRAME_HEIGHT 600
 #define MIN_CAM_X -CAM_FRAME_WIDTH / 2
 #define MAX_CAM_X CAM_FRAME_WIDTH / 2
 #define SERVO_MIN 0
 #define SERVO_MAX 180
 
 Servo servo;
-volatile int servoRotation = 90;
+volatile double servoRotation = 90;
 
 void setup() {
   servo.attach(SERVO_PIN);
@@ -26,8 +27,9 @@ void setup() {
 }
 
 void loop() {
-//    Serial.println(servoRotation);
-    servo.write(servoRotation);
+    servo.write((int)servoRotation);
+    
+    delay(10);
 }
 
 // numBytes: total number of bytes
@@ -42,17 +44,17 @@ void onReceiveI2C(int numBytes) {
     int value = get16BitInt();
     Serial.println(value);
 
-//    int servoRotation = map(value, MIN_CAM_X, MAX_CAM_X, SERVO_MIN, SERVO_MAX);
-//    servo.write(servoRotation);
-    if(value < 0) {
+    if(value > -5 && value < 5) {
+        return;
+    } else if(value < 0) {
       if(servoRotation < 180) {
-        servoRotation += 2;
+        servoRotation -= (double)value / 90;
       }
     } else if(value > 0) {
       if(servoRotation > 0) {
-        servoRotation -= 2;
+        servoRotation -= (double)value / 90;
       }  
-   }
+    }
   }
 }
 
