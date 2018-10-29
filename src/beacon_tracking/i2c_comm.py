3b672d673c0dba92e9a238639aa985cca4cff9d5
erbornemeier <erbornemeier@mines.edu>
2018-10-28 21:48:56 -0600

    made communication class more general such that flags are determined abstract to the communcation class

---
+++ src/beacon_tracking/i2c_comm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import smbus
+
+class i2c_comm:
+
+    def __init__(self, addr, bus=1, max_send_attempts=3):
+        self.addr = addr
+        self.bus = smbus.SMBus(bus)
+        self.max_attempts = max_send_attempts
+
+    def send_cmd(self, flag, data):
+        for attempt in range(max_send_attempts):
+            try:
+                msg = self.serializeMsg(list(pos))
+                self.bus.write_block_data(self.addr, flag, msg)
+                return True
+            except IOError:
+                print("Attempt {}: can't communicate with arduino".format(attempt))
+        return False
+
+    def serializeMsg(self, msg):
+        new_msg = []
+       	for num in msg:
+             new_msg.append(num >> 8 & 0xff)
+             new_msg.append(num & 0xff)
+        return new_msg
+
+
+# Use this to debug this connection
+if __name__ == "__main__":
+    try:
+        comm = i2c_comm(0x28)
+        i = -1
+        while i != -1:
+            i = int(input())
+            comm.sendLEDPos((i, i+2))
+
+    except KeyboardInterrupt:
+        pass
+
