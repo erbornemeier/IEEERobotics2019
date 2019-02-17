f189cd269fff371d1be75268e7eea0f7fef3eb44
Easton Bornemeier <erbornemeier@mines.edu>
2019-02-17 08:06:35 +0000

    uses arduino speed controller for block pickup

--- ros_catkin_ws/src/scheduler/src/scheduler.py
+++ ros_catkin_ws/src/scheduler/src/scheduler.py
@@ -5,13 +5,16 @@ from std_msgs.msg import UInt8
 from geometry_msgs.msg import Pose2D
 from state_machine.state_machine import StateMachine
 from state_machine.drive_to_block_state import DriveToBlockState
+from state_machine.pick_up_block_state import PickUpBlockState
 import time as t
 
 rospy.init_node("scheduler")
 
 state_machine = StateMachine(DriveToBlockState())
 
+t.sleep(5)
 _ = raw_input("Press enter to start")
+
 
 while not rospy.is_shutdown():
     state_machine.run()
