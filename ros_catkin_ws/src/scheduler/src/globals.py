ffb430c8bbc21ccfe6926cc50bf57642b1d6a5c4
erbornemeier <erbornemeier@mines.edu>
2019-03-05 16:51:06 -0700

    added test json parser

--- ros_catkin_ws/src/scheduler/src/globals.py
+++ ros_catkin_ws/src/scheduler/src/globals.py
@@ -1,3 +1,5 @@
-
-
 current_letter = -1
+
+num_blocks = -1
+x_coords = []
+y_coords = []
--- ros_catkin_ws/src/scheduler/src/scheduler.py
+++ ros_catkin_ws/src/scheduler/src/scheduler.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
+import subprocess
+import json
+import re
+
 from std_msgs.msg import UInt8
 from geometry_msgs.msg import Pose2D
 from state_machine.state_machine import StateMachine
@@ -8,14 +12,37 @@ from state_machine.drive_to_block_state import DriveToBlockState
 from state_machine.pick_up_block_state import PickUpBlockState
 from state_machine.drive_to_mothership_state import DriveToMothershipState
 import time as t
+import globals
 
+def wait_for_flash_drive():
+    while True:
+        blkids = subprocess.check_output(['ls','/dev/'])
+        m = re.search('(/dev/sd[a-z]1)', blkids) 
+        print(blkids)
+        if m:
+            print(m.group(0))
+            break
+        t.sleep(1)
+
+    path = 'mar1.json'
+    with open(path, 'r') as f:
+        data = f.read()
+    data = json.loads(data)
+    globals.num_blocks = data['size']
+    globals.x_coords = data['x coords']
+    globals.y_coords = data['y coords']
+    while (1):
+        pass
+'''
 rospy.init_node("scheduler")
 
 state_machine = StateMachine(DriveToBlockState())
 
-t.sleep(5)
 _ = raw_input("Press enter to start")
-
+'''
+wait_for_flash_drive()
+while (1):
+    pass
 
 while not rospy.is_shutdown():
     state_machine.run()
