003c3414c62f203bab1b6a8cf20fde1a0a9195c9
ZKasica <zkasica@mymail.mines.edu>
2019-03-21 15:58:18 -0600

    Fix typo

--- ros_catkin_ws/src/scheduler/src/state_machine/determine_mothership_orientation_state.py
+++ ros_catkin_ws/src/scheduler/src/state_machine/determine_mothership_orientation_state.py
@@ -33,7 +33,7 @@ class DetermineMothershipOrientationState(State):
             detected_slot = Letter()
             detected_slot.letter = 0
 
-            commands.display_letter(1 if detected_slot.letter == 0 else 5)
+            commands.display_letter(1 if detected_slot.letter == 0 else 4)
             commands.set_display_state(commands.LETTER)
 
             if detected_slot != 0xFF:
@@ -41,7 +41,7 @@ class DetermineMothershipOrientationState(State):
                 globals.mothership_theta = self.robot_theta if detected_slot.letter == 0 else 180 - self.robot_theta
                 globals.mothership_x = self.robot_x
                 globals.mothership_y = self.robot_y
-                rospy.loginfo("Detected:", "ABC" if detected_slot.letter == 0 else "DEF", "  MOTHERSHIP ORIENTATION: ", globals.mothership_theta, "POSITION:", self.mothership_x, " ", self.mothership_y)
+                rospy.loginfo("Detected:", "ABC" if detected_slot.letter == 0 else "DEF", "  MOTHERSHIP ORIENTATION: ", globals.mothership_theta, "POSITION:", globals.mothership_x, " ", globals.mothership_y)
 
                 commands.send_drive_forward_command(-10)
                 rospy.Rate(0.2).sleep()
