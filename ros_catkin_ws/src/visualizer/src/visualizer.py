dead6f4a1b1d2ce72699897eaa6b8c74f2737525
ZKasica <zkasica@mymail.mines.edu>
2019-03-23 14:55:28 -0600

    Add visualizer basics

---
+++ ros_catkin_ws/src/visualizer/src/visualizer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+import subprocess
+import json
+import re
+import time
+
+from socket import *
+
+from std_msgs.msg import UInt8
+from geometry_msgs.msg import Pose2D
+
+rospy.init_node("visualizer")
+server_ip = rospy.get_param("/visualizer/server_ip")
+server_port = rospy.get_param("/visualizer/server_port")
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+client_socket.settimeout(1)
+addr = (server_ip, int(server_port))
+
+# isConnected = False
+# attempts = 3
+# while attempts > 0:
+#     client_socket.sendto("initialize", addr)
+
+#     try:
+#         data, server = clientSocket.recvfrom(1024)
+#         isConnected = true
+#         print("[Visualizer] Connected to server")
+#     except:
+#         print("Not connected")
+#         time.sleep(1)
+#         attempts -= 1
+    
+# if not isConnected:
+#     print("Continuing without visualizer")
+#     exit()
+
+client_socket.sendto("init-robot", addr)
+
+def sendPose(self, msg):
+    client_socket.sendto("update-robot-pose x:{} y:{} theta:{}", msg.x, msg.y, msg.theta)
+
+pose_sub = rospy.Subscriber('robot_pose', Pose2D, self.sendPose)
+
+while not rospy.is_shutdown():
+    pass
