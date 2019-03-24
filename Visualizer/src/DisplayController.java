19b8e8b9c2a7f30c88973e96a536d0ebfca11d71
ZKasica <zkasica@mymail.mines.edu>
2019-03-23 23:06:17 -0600

    Update regex in display controller

--- Visualizer/src/DisplayController.java
+++ Visualizer/src/DisplayController.java
@@ -41,7 +41,7 @@ public class DisplayController {
         Pattern p = Pattern.compile(
             ".*\\s*x:(\\d*\\.?\\d*)"
             + "\\s*y:(\\d*\\.?\\d*)"
-            + "-?\\s*theta:(\\d*\\.?\\d*)\\s*"
+            + "\\s*-?theta:(\\d*\\.?\\d*)"
         );
 
         Matcher m = p.matcher(msg);
