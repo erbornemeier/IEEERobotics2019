88edfacb94cd20dc68da0bf87f15c7c28b62cc39
erbornemeier <erbornemeier@mines.edu>
2019-04-01 14:58:25 -0600

    updated block detection to distinguish between blocks and other things

--- ros_catkin_ws/src/object_detection/src/block_position_finder.py
+++ ros_catkin_ws/src/object_detection/src/block_position_finder.py
@@ -21,7 +21,7 @@ def image_recieved(data):
     global height
     img = bridge.imgmsg_to_cv2(data, "bgr8")
     width, height = len(img[0]), len(img)
-
+'''
 def get_block_pos(_):
     response = BlockResponse()
     try:
@@ -46,6 +46,85 @@ def get_block_pos(_):
             screenY = int(M["m01"] / M["m00"])
             response.x, response.y = screenX / float(width), screenY / float(height)
             return response
+
+    except Exception as e:
+        #print (e)
+        response.x = -1
+        response.y = -1
+        return response
+'''
+
+diff_weight = 1
+aspect_weight = 20
+min_goodness = 35
+
+def average_contrast(img, contour, debug=False):
+    x, y, w, h = cv2.boundingRect(contour)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    black = np.zeros(img.shape[:2], dtype=np.uint8)
+    cv2.drawContours(black, [contour], 0, (255,255,255), thickness=cv2.FILLED)
+    cropped = cv2.bitwise_and(gray, black)
+    if cropped.size == 0:
+        return 0
+    avg_pixel_value = np.sum(cropped) / np.count_nonzero(cropped)
+
+    diff = cv2.bitwise_and(black.astype(np.float64), np.abs(cropped - avg_pixel_value))
+    pixel_diffs = np.sum(diff)
+    avg_pixel_diff = pixel_diffs / np.count_nonzero(diff)
+    aspect_error = max(w/float(h), h/float(w))-1
+    goodness =  avg_pixel_diff*diff_weight - aspect_error*aspect_weight 
+    if debug:
+        cv2.putText(img, str(round(goodness, 3)), (x+w//2, y+h//2),\
+                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,0,255), 2)
+    return goodness
+
+def get_block_contour(img, debug=False):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)                              
+                                                                            
+    orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
+    orange_mask = cv2.morphologyEx(orange_mask, cv2.MORPH_OPEN, kernel)
+    orange_mask = cv2.morphologyEx(orange_mask, cv2.MORPH_CLOSE, kernel)
+    orange_mask = cv2.erode(orange_mask, kernel, iterations=1)
+    _, contours, hier = cv2.findContours(orange_mask, cv2.RETR_TREE, 
+                                                      cv2.CHAIN_APPROX_SIMPLE) 
+    interiors = []
+    maxIndex = np.argmax([cv2.contourArea(c) for c in contours])
+    AREA_THRESH = 100
+    for i, h in enumerate(hier[0]):
+        if h[3] == maxIndex:
+            if cv2.contourArea(contours[i]) > AREA_THRESH:
+                interiors.append(contours[i])
+
+    if len(interiors) > 0:
+        goodness_values = [(i, average_contrast(img, i, debug=debug)) for i in interiors]
+        max_contrast = max(goodness_values, key=lambda x: x[1])
+        if debug:
+            cv2.imshow('mask', orange_mask)
+        if max_contrast[1] > min_goodness:
+            return max_contrast[0]
+
+    non_orange_mask = cv2.bitwise_not(orange_mask)
+    _, contours, _ = cv2.findContours(non_orange_mask, cv2.RETR_EXTERNAL, 
+                                                  cv2.CHAIN_APPROX_SIMPLE) 
+    goodness_values = [(c, average_contrast(img, c, debug=debug)) for c in contours]
+    max_contrast = max(goodness_values, key=lambda x: x[1])
+    if debug:
+        cv2.imshow('no_mask', non_orange_mask)
+    if max_contrast[1] > min_goodness:
+        return max_contrast[0]
+    return None
+
+def get_block_pos(_):
+    response = BlockResponse()
+    try:
+        block = get_block_contour(img)
+        if block is None:
+            raise Exception('no block found')
+        M = cv2.moments(block)
+        screenX = int(M["m10"] / M["m00"])
+        screenY = int(M["m01"] / M["m00"])
+        response.x, response.y = screenX / float(width), screenY / float(height)
+        return response
 
     except Exception as e:
         #print (e)
--- src/block_detection/current_block_detection.py
+++ src/block_detection/current_block_detection.py
@@ -1,41 +1,84 @@
 import cv2
 import numpy as np
 
-orange_lower = np.array([4,100,0])                                              
+orange_lower = np.array([4,50,100])                                              
 orange_upper = np.array([18,255,255])                                           
-white_lower = np.array([0,0,160])                                               
+white_lower = np.array([0,0,140])                                               
 white_upper = np.array([255,255,255])                                           
 kernel = np.ones((5,5),np.uint8)                                                
 kernel2 = np.ones((5,5),np.uint8) 
+kernel3 = np.ones((10,10),np.uint8) 
 
 cap = cv2.VideoCapture(1)
 
-while True:
-    _, img = cap.read()
+diff_weight = 1
+aspect_weight = 20
+min_goodness = 35
+
+def average_contrast(img, contour, debug=False):
+    x, y, w, h = cv2.boundingRect(contour)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    black = np.zeros(img.shape[:2], dtype=np.uint8)
+    cv2.drawContours(black, [contour], 0, (255,255,255), thickness=cv2.FILLED)
+    cropped = cv2.bitwise_and(gray, black)
+    if cropped.size == 0:
+        return 0
+    avg_pixel_value = np.sum(cropped) / np.count_nonzero(cropped)
+
+    diff = cv2.bitwise_and(black.astype(np.float64), np.abs(cropped - avg_pixel_value))
+    pixel_diffs = np.sum(diff)
+    avg_pixel_diff = pixel_diffs / np.count_nonzero(diff)
+    aspect_error = max(w/float(h), h/float(w))-1
+    goodness =  avg_pixel_diff*diff_weight - aspect_error*aspect_weight 
+    if debug:
+        cv2.putText(img, str(round(goodness, 3)), (x+w//2, y+h//2),\
+                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,0,255), 2)
+    return goodness
+
+def get_block_contour(img, debug=False):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)                              
                                                                             
     orange_mask = cv2.inRange(hsv, orange_lower, orange_upper)
+    orange_mask = cv2.morphologyEx(orange_mask, cv2.MORPH_OPEN, kernel)
+    orange_mask = cv2.morphologyEx(orange_mask, cv2.MORPH_CLOSE, kernel)
+    orange_mask = cv2.erode(orange_mask, kernel, iterations=1)
+    _, contours, hier = cv2.findContours(orange_mask, cv2.RETR_TREE, 
+                                                      cv2.CHAIN_APPROX_SIMPLE) 
+    interiors = []
+    maxIndex = np.argmax([cv2.contourArea(c) for c in contours])
+    AREA_THRESH = 100
+    for i, h in enumerate(hier[0]):
+        if h[3] == maxIndex:
+            if cv2.contourArea(contours[i]) > AREA_THRESH:
+                interiors.append(contours[i])
+
+    if len(interiors) > 0:
+        goodness_values = [(i, average_contrast(img, i, debug=debug)) for i in interiors]
+        max_contrast = max(goodness_values, key=lambda x: x[1])
+        if debug:
+            cv2.imshow('mask', orange_mask)
+        if max_contrast[1] > min_goodness:
+            return max_contrast[0]
+
     non_orange_mask = cv2.bitwise_not(orange_mask)
-    non_orange_mask = cv2.morphologyEx(non_orange_mask, cv2.MORPH_OPEN, kernel)
-    non_orange_mask = cv2.morphologyEx(non_orange_mask, cv2.MORPH_CLOSE, kernel)
+    _, contours, _ = cv2.findContours(non_orange_mask, cv2.RETR_EXTERNAL, 
+                                                  cv2.CHAIN_APPROX_SIMPLE) 
+    goodness_values = [(c, average_contrast(img, c, debug=debug)) for c in contours]
+    max_contrast = max(goodness_values, key=lambda x: x[1])
+    if debug:
+        cv2.imshow('no_mask', non_orange_mask)
+    if max_contrast[1] > min_goodness:
+        return max_contrast[0]
+    return False
+         
 
-    white_mask = cv2.inRange(hsv, white_lower, white_upper)
-    white_mask = cv2.morphologyEx(white_mask, cv2.MORPH_CLOSE, kernel)
-    white_mask = cv2.morphologyEx(white_mask, cv2.MORPH_OPEN, kernel)
-    mask = cv2.bitwise_and(white_mask, non_orange_mask)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel2)
-
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if (len(contours) > 0):
-        block = max(contours, key=cv2.contourArea)
-        M = cv2.moments(block)
-        screenX = int(M["m10"] / M["m00"])
-        screenY = int(M["m01"] / M["m00"])
-
-        cv2.imshow('white_mask', white_mask)
-        cv2.imshow('non-orange-mask', non_orange_mask)
-        cv2.imshow('mask', mask)
-
+while True:
+    _, img = cap.read()
+    block = get_block_contour(img, debug=True)
+    if block is not False:
+        cv2.drawContours(img, [block], 0, (0,255,0), 3)
+        
+    cv2.imshow('img', img)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
