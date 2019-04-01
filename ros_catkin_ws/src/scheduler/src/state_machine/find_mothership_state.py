467e874d7dc5fb39ee571200729eb996d00b9231
Easton Bornemeier <erbornemeier@mines.edu>
2019-04-01 07:25:53 +0000

    get robot to find mothership when leds arent visible

--- ros_catkin_ws/src/scheduler/src/state_machine/find_mothership_state.py
+++ ros_catkin_ws/src/scheduler/src/state_machine/find_mothership_state.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Pose2D
 from object_detection.srv import *
 import commands
 import drive_utils
+import geometry_utils
 import time as t
 import rospy
 from math import *
@@ -18,7 +19,7 @@ class FindMothershipState(State):
     def start(self):
         super(FindMothershipState, self).start()
 
-        commands.send_drive_vel_command(0, 0.8)
+        commands.send_drive_vel_command(0, 0.6)
         commands.send_cam_command(15)
         commands.send_claw_command(commands.CARRY_ANGLE)
         commands.set_display_state(commands.NORMAL)
@@ -28,8 +29,8 @@ class FindMothershipState(State):
         self.angle_of_max = 0
 
         self.start_time = t.time()
-        self.timeout = 40
-        self.drive_dst = 36
+        self.timeout = 27
+        self.drive_dst = 40
 
     def __get_mothership_pos__(self):
         # Coordinate system [0,1] top left corner is (0,0)
@@ -58,10 +59,23 @@ class FindMothershipState(State):
         mothership_pos = self.__get_mothership_pos__()
 
         if t.time() - self.start_time > self.timeout:
-            target_angle = self.angle_of_max - 45
+            commands.send_drive_vel_command(0,0)
+
+            target_angle = self.angle_of_max - 60
             pt_x = 48 + self.drive_dst * cos(radians(target_angle))
             pt_y = 48 + self.drive_dst * sin(radians(target_angle))
+
+            tmp_bad_points = []
+            for p in drive_utils.grid:
+                if geometry_utils.pointInEllipse(48, 48, self.angle_of_max, p[0], p[1], 100, 30) \
+                    and not geometry_utils.pointInEllipse(48, 48, self.angle_of_max, p[0], p[1], 12, 30):
+                    tmp_bad_points.append(p)
+                    globals.bad_points.add(p)
+
             drive_utils.go_to_point((pt_x, pt_y))
+
+            for p in tmp_bad_points:
+                globals.bad_points.remove(p)
             return FindMothershipState('if you are reading this help me')
 
 
